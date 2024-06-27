d189610c5e96ee074251ca883349a40d2a0db9c9
Christophe <christophe.misc+git@protonmail.ch>
2024-06-27 09:56:48 +0200

    fix: buildUrl missing route, oops

--- src/operationToFunction/buildUrl.ts
+++ src/operationToFunction/buildUrl.ts
@@ -6,11 +6,12 @@ const buildUrl = (route: string, parameters: Parameter[]) =>
   template.concat(
     // eslint-disable-next-line no-template-curly-in-string
     "const url = `${config?.basePath ?? ''}",
-    parameters.length > 0 &&
-      substituteParams(
-        route,
-        parameters.filter((param) => param.in === 'path'),
-      ),
+    parameters.length > 0
+      ? substituteParams(
+          route,
+          parameters.filter((param) => param.in === 'path'),
+        )
+      : route,
     '`;',
   );
 
