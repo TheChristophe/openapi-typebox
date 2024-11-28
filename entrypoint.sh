42394f277be8cf80736fca9aef9b0b25dbe57bd4
Christophe <christophe.misc+git@protonmail.ch>
2024-11-28 03:17:31 +0100

    build: fix permissions on client folder to allow for creating folders within
    TODO: what permissions are default, and why are they not readable by github actions?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /app || exit
 
-mkdir -p "${OUT_DIR}"
+mkdir -p -m 777 "${OUT_DIR}"
 # pretend the output folder is local
 # this required for two reasons:
 # - eslint will ignore files outside the cwd,
